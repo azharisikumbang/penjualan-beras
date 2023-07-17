86fb49bfa445f138647a6fabc890f36a905f9b62
Azhari <azharisaputrasikumbang@gmail.com>
2023-07-18 01:09:14 +0700

    schema database

---
+++ src/resources/database.sql
@@ -0,0 +1,99 @@
+CREATE TABLE akun (
+    id int primary key auto_increment,
+    username varchar(255) unique not null,
+    password varchar(255) not null,
+    role varchar(32) default 'pelanggan',
+    created_at DATETIME default CURRENT_TIMESTAMP
+);
+
+CREATE TABLE beras (
+    id int primary key auto_increment,
+    jenis varchar(128) not null,
+    harga decimal(10, 2) default 0,
+    stok int default 0,
+    created_at DATETIME default CURRENT_TIMESTAMP,
+    updated_at DATETIME default CURRENT_TIMESTAMP
+);
+
+CREATE TABLE karyawan (
+    id int primary key auto_increment,
+    nama varchar(255) not null,
+    kontak varchar(32),
+    jabatan varchar(32),
+    akun_id int,
+
+    FOREIGN KEY (akun_id) REFERENCES akun(id)
+);
+
+CREATE TABLE pelanggan (
+  id int primary key auto_increment,
+  nama varchar(255) not null,
+  kontak varchar(32),
+  alamat varchar(255),
+  akun_id int,
+
+  FOREIGN KEY (akun_id) REFERENCES akun(id)
+);
+
+CREATE TABLE pesanan(
+    id int primary key auto_increment,
+    nomor_pesanan varchar(128) not null unique,
+    nomor_iterasi_pesanan int not null,
+    nama_pesanan varchar(255) not null,
+    alamat_pengiriman varchar(255) not null,
+    tanggal_pemesanan DATETIME,
+    total_tagihan decimal(10, 2) default 0,
+    pemesan_id int not null,
+
+    FOREIGN KEY (pemesan_id) REFERENCES pelanggan(id)
+);
+
+CREATE TABLE detail_pesanan(
+  id int primary key auto_increment,
+  jenis_beras varchar(255) not null,
+  harga_satuan decimal(10, 2) default 0,
+  jumlah_beli int default 1,
+  total decimal(10, 2) default 0,
+  pesanan_id int not null,
+
+  FOREIGN KEY (pesanan_id) REFERENCES pesanan(id) ON DELETE CASCADE
+);
+
+CREATE TABLE transaksi(
+    id int primary key auto_increment,
+    tanggal_pembayaran DATETIME not null,
+    nama_pembayaran varchar(255),
+    bank_pembayaran varchar(255),
+    nominal_dibayarkan decimal(10, 2) default 0,
+    status_pembayaran varchar(16) not null ,
+    konfirmasi_pembayaran varchar(16) not null,
+    file_bukti_pembayaran varchar(255),
+    pesanan_id int not null,
+
+    FOREIGN KEY (pesanan_id) REFERENCES pesanan(id) ON DELETE CASCADE
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
