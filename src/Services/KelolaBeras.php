04cf6f5c488ddc7c8a2f8b645c3ea034283fdbd9
Azhari <azharisaputrasikumbang@gmail.com>
2023-07-20 16:10:35 +0700

    service kelola beras

---
+++ src/Services/KelolaBeras.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once __DIR__ . '/../Repositories/BerasRepository.php';
+require_once __DIR__ . '/../Entities/Beras.php';
+
+class KelolaBeras
+{
+
+    private BerasRepository $berasRepository;
+
+    public function __construct()
+    {
+        $this->berasRepository = new BerasRepository();
+    }
+
+    public function tambahkanBerasBaru(string $jenis, int|float $harga, int $stok): false|Beras
+    {
+        if ($harga < 0 || $stok < 0) return false;
+
+        $beras = new Beras();
+        $beras->setJenis($jenis);
+        $beras->setHarga($harga);
+        $beras->setStok($stok);
+
+        return $this->berasRepository->save($beras);
+    }
+
+    public function perbaharuiDataBeras(int $id, ?string $jenis = null, null|int|float $harga = null, ?int $stok): false|Beras
+    {
+        $beras = $this->berasRepository->findById($id);
+        if (is_null($beras))  return false;
+
+        $updatable = [];
+        if($jenis) $updatable['jenis'] = $jenis;
+        if($harga) {
+            if($harga < 0) return false;
+
+            $updatable['harga'] = $harga;
+        }
+
+        if($stok) {
+            if ($stok < 0) return false;
+
+            $updatable['stok'] = $stok;
+        }
+
+        return $this->berasRepository->update($beras, $updatable);
+    }
+
+    public function hapusDataBeras(int $id): bool
+    {
+        return $this->berasRepository->remove($id);
+    }
+
+    public function rubahStokBeras(int $id, int $stokBaru): false|Beras
+    {
+        $beras = $this->berasRepository->findById($id);
+        if (is_null($beras))  return false;
+
+        $updatable = ['stok' => $beras->getStok()];
+        if($stokBaru) {
+            if ($stokBaru < 0) return false;
+
+            $updatable['stok'] = $stokBaru;
+        }
+
+        return $this->berasRepository->update($beras, $updatable);
+    }
+
+    public function listBeras(int $total = 10, int $start = 0): array
+    {
+        return $this->berasRepository->get($total, $start, 'jenis', 'ASC');
+    }
+}
