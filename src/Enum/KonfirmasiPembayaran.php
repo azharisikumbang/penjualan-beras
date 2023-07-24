56436b9656581a74b1523b52c3caf987169bb656
Azhari <azharisaputrasikumbang@gmail.com>
2023-07-24 16:39:38 +0700

    change value to BELUM_BAYAR

--- src/Enum/KonfirmasiPembayaran.php
+++ src/Enum/KonfirmasiPembayaran.php
@@ -2,7 +2,7 @@
 
 enum KonfirmasiPembayaran: string
 {
-    case BELUM_DIKONFIRMASI = 'BELUM_DIKONFIRMASI';
+    case BELUM_BAYAR = 'BELUM_BAYAR';
 
     case MENUNGGU_KONFIRMASI = 'MENUNGGU_KONFIRMASI';
 
