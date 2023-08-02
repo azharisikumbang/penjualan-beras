8b04d178f5ae55f5aa0c0f4ecf11ab6430ce42d3
Azhari <azharisaputrasikumbang@gmail.com>
2023-08-03 02:34:23 +0700

    add: method untuk menampilkan teks readble dan warna dasar tampilan

--- src/Enum/KonfirmasiPembayaran.php
+++ src/Enum/KonfirmasiPembayaran.php
@@ -9,4 +9,24 @@ enum KonfirmasiPembayaran: string
     case DITOLAK = 'DITOLAK';
 
     case DITERIMA = 'DITERIMA';
+
+    public function getDisplay()
+    {
+        return match ($this) {
+            self::MENUNGGU_KONFIRMASI => 'MENUNGGU KONFIRMASI ADMIN',
+            self::DITOLAK => 'DITOLAK',
+            self::DITERIMA => 'DITERIMA',
+            default => 'BELUM BAYAR'
+        };
+    }
+
+    public function getColor()
+    {
+        return match ($this) {
+            self::MENUNGGU_KONFIRMASI => 'yellow',
+            self::DITOLAK => 'red',
+            self::DITERIMA => 'green',
+            default => 'gray'
+        };
+    }
 }
