8b089ba989a5f2b217cff9583e88db0b127ddbfb
Azhari <azharisaputrasikumbang@gmail.com>
2023-08-05 16:43:51 +0700

    add entity interface

--- src/Contract/RepositoryInterface.php
+++ src/Contract/RepositoryInterface.php
@@ -1,12 +1,14 @@
 <?php
 
+require_once __DIR__ . '/EntityInterface.php';
+
 interface RepositoryInterface
 {
     public function exists(int $id): bool;
 
     public function findById(int $ind): ?EntityInterface;
 
-    public function findBy(string $column, string $agg = '=', mixed $value = null): ?EntityInterface;
+    public function findBy(string $column, mixed $value, string $agg = '='): ?EntityInterface;
 
     public function get(int $from = 0, int $total = 10, string $order = 'order', string $by = 'id') : array;
 
