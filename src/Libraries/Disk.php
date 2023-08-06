0b187891264433f2ecff9a141262ce1bb2f779d5
Azhari <azharisaputrasikumbang@gmail.com>
2023-08-06 22:12:13 +0700

    fix: gambar yang diterima untuk bukti pembayaran hanya type image/*

--- src/Libraries/Disk.php
+++ src/Libraries/Disk.php
@@ -4,6 +4,8 @@ class Disk
 {
     public static function simpanBuktiPembayaran(array $files): false|string
     {
+        if (false === self::isImage($files['tmp_name'])) return false;
+
         $location = config('disk', 'bukti_pembayaran');
         $ext = pathinfo($files['name'], PATHINFO_EXTENSION);
         $filename = strtolower(md5($files['name'] . microtime()) . "." . $ext);
@@ -12,5 +14,10 @@ class Disk
         if(false === move_uploaded_file($files['tmp_name'], $target)) return false;
 
         return $filename;
+    }
+
+    private static function isImage(string $filename): false|int
+    {
+        return exif_imagetype($filename);
     }
 }
