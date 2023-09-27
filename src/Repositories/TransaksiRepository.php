88929a6095b71d8291b7eb33e64f4deeb3df3546
Kum <azharisaputrasikumbang@gmail.com>
2023-09-27 19:51:08 +0700

    add: display 'laporan pemasukan berdasarkan periode'

--- src/Repositories/TransaksiRepository.php
+++ src/Repositories/TransaksiRepository.php
@@ -46,4 +46,75 @@ class TransaksiRepository extends BaseRepository
             'pesanan_id' => $pesanan->getId()
         ]);
     }
+
+
+    public function getDataForLaporanPemasukan(int $year, int $month = 0, int $date = 0): array
+    {
+        $type = 'YEAR';
+        $where = "YEAR(tanggal_pembayaran) = $year";
+        $group = 'MONTH(tanggal_pembayaran)';
+
+        if ($month > 0) {
+            $type = 'MONTH';
+            $where = sprintf("YEAR(tanggal_pembayaran) = '%s' AND MONTH(tanggal_pembayaran) = '%s'", $year, $month);
+            $group = "MONTH(tanggal_pembayaran)";
+        }
+
+        if ($date > 0) {
+            $type = 'DATE';
+            $where = sprintf("DATE(tanggal_pembayaran) = '%s-%s-%s'", $year, $month, $date);
+            $group = 'DATE(tanggal_pembayaran)';
+        }
+
+        $query = "SELECT '{$type}' as type, {$group} as periode, SUM(nominal_dibayarkan) as total
+            FROM {$this->getTable()}
+            WHERE konfirmasi_pembayaran = 'DITERIMA' AND {$where}
+            GROUP BY {$group}";
+
+        $stmt = $this->execute($query);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getDataForLaporanPenjualan(int $year, int $month = 0, int $date = 0): array
+    {
+        $type = 'YEAR';
+        $where = "YEAR(tanggal_pembayaran) = $year";
+        $group = 'MONTH(tanggal_pembayaran)';
+
+        if ($month > 0) {
+            $type = 'MONTH';
+            $where = sprintf("YEAR(tanggal_pembayaran) = '%s' AND MONTH(tanggal_pembayaran) = '%s'", $year, $month);
+            $group = "MONTH(tanggal_pembayaran)";
+        }
+
+        if ($date > 0) {
+            $type = 'DATE';
+            $where = sprintf("DATE(tanggal_pembayaran) = '%s-%s-%s'", $year, $month, $date);
+            $group = 'DATE(tanggal_pembayaran)';
+        }
+
+        $query = "SELECT 
+                '{$type}' as type, 
+                {$group} as periode, b.jenis, 
+                vt.variant, SUM(dp.jumlah_beli) as 'terjual',
+                b.id as b_beras_id,
+                vt.id as vt_variant_id
+            FROM (SELECT id, pesanan_id, tanggal_pembayaran FROM transaksi WHERE konfirmasi_pembayaran = 'DITERIMA' AND {$where}) t
+            JOIN pesanan p on p.id = t.pesanan_id
+            JOIN detail_pesanan dp on p.id = dp.pesanan_id
+            JOIN beras b on b.id = dp.ref_beras_id
+            JOIN varian_takaran vt on vt.id = dp.ref_takaran_id
+            GROUP BY b.jenis, vt.variant, b.id, vt.id, {$group}
+            ORDER BY periode, b.jenis, vt.variant";
+
+
+        try {
+            $stmt = $this->execute($query);
+
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (\PDOException $exception) {
+            return [];
+        }
+    }
 }
