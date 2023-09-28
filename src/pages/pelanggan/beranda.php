beee4d8d239302e7ccfa5ef0f89bfefafca25dc9
Kum <azharisaputrasikumbang@gmail.com>
2023-09-28 20:32:36 +0700

    fix: perbaiki teks yg ditampilkan ke user

--- src/pages/pelanggan/beranda.php
+++ src/pages/pelanggan/beranda.php
@@ -7,6 +7,7 @@ if (false === session()->isAuthenticatedAs('pelanggan')) html_unauthorized();
     <div class="p-6 bg-white rounded-lg border">
         <div class="mb-4 col-span-full xl:mb-2">
             <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">Beranda</h1>
+            <p>Selamat datang, untuk melihat riwayat pemesanan anda ada di menu 'Riwayat Pembelian' atau silahkan buat pembelian baru di menu Pesan Beras.</p>
         </div>
     </div>
 </main>
