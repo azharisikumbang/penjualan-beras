12b73016b13a331170671d4aafac400406605d22
Kum <azharisaputrasikumbang@gmail.com>
2025-03-18 15:04:08 +0700

    remove dark mode from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/**/*.{html,js,php}", "./src/templates/**/*.{html,js,php}"],
+  content: [
+    "./src/pages/**/*.{html,js,php}",
+    "./src/templates/**/*.{html,js,php}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+  darkMode: "false",
+};
